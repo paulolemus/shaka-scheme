2963dec1774d3fed66fa6d7eb8c14540b60fd430
Paulo Lemus <paulolemus14@gmail.com>
2018-03-10 02:35:49 -1000

    Wrote headers for SyntaxRulesMacro and SyntaxRule

---
+++ src/shaka_scheme/system/parser/syntax_rules/SyntaxRule.hpp
@@ -0,0 +1,95 @@
+#ifndef SHAKA_SCHEME_SYNTAXRULE_HPP
+#define SHAKA_SCHEME_SYNTAXRULE_HPP
+
+#include <set>
+
+#include "shaka_scheme/system/base/DataPair.hpp"
+#include "shaka_scheme/system/base/Symbol.hpp"
+
+namespace shaka {
+namespace macro {
+
+/**
+ * @brief The class used for matching to and expanding a single <syntax-rule>
+ * as defined in R7RS.
+ *
+ * How it works:
+ * 1. build() is called. This builds the internal function that is used for
+ * matching and extracting binding information from a macro use.
+ * 2. expand(NodePtr macro). This binds all data from the macro pattern, and
+ * then modifies the NodePtr location to expand transform it into the filled
+ * template version.
+ *
+ * TODO: Create builder class for SyntaxRule.
+ */
+class SyntaxRule {
+
+public:
+
+  /**
+   * @brief Create SyntaxRule. TODO: BUILDER CLASS
+   * @param ellipsis Ellipsis identifier <ellipsis>.
+   * @param literal_ids Set of special IDs that can be matched but not bound.
+   * @param pattern NodePtr to <pattern> of <syntax rule>.
+   * @param templat NodePtr to <template> of <syntax rule>.
+   */
+  SyntaxRule(
+      Symbol& ellipsis,
+      std::set<Symbol>& literal_ids,
+      NodePtr pattern,
+      NodePtr templat
+  );
+
+  /**
+   * @brief Build the internal pattern matching and binding extracting functor.
+   * Can potentially throw a MacroExpansionException.
+   */
+  void build();
+
+
+  /**
+   * @brief Match a macro use to this particular SyntaxRule pattern. On a
+   * successful match, all bindings are extracted but not used.
+   * @param macro NodePtr to the macro AST.
+   * @return true on successful match, false otherwise.
+   */
+  bool match(NodePtr macro);
+
+  /**
+   * @brief Match and then expand a macro use if it matches the <pattern> of
+   * this SyntaxRule. On failure to match, this function does nothing.
+   * @param macro NodePtr to head of the macro use AST.
+   * @return true on successful match and expansion, false otherwise.
+   */
+  bool transform(NodePtr macro);
+
+  /**
+   * @brief Prints a representation of SyntaxRule.
+   * @param lhs The output stream.
+   * @param rhs The SyntaxRule reference.
+   * @return The updated reference of the output stream.
+   */
+  friend std::ostream& operator<<(
+      std::ostream& lhs,
+      const SyntaxRule& rhs
+  );
+
+private:
+
+  const Symbol ellipsis;
+  const std::set<Symbol> literal_ids;
+  const NodePtr pattern;
+  const NodePtr templat;
+
+};
+
+/**
+ * @brief Alias for type.
+ */
+using SyntaxRulePtr = std::shared_ptr<SyntaxRule>;
+
+}
+}
+
+
+#endif //SHAKA_SCHEME_SYNTAXRULE_HPP
--- src/shaka_scheme/system/parser/syntax_rules/SyntaxRulesMacro.hpp
+++ src/shaka_scheme/system/parser/syntax_rules/SyntaxRulesMacro.hpp
@@ -2,6 +2,11 @@
 #define SHAKA_SCHEME_SYNTAXRULESMACRO_HPP
 
 #include <memory>
+#include <vector>
+
+#include "shaka_scheme/system/base/DataPair.hpp"
+#include "shaka_scheme/system/base/Symbol.hpp"
+#include "shaka_scheme/system/parser/syntax_rules/SyntaxRule.hpp"
 
 namespace shaka {
 namespace macro {
@@ -16,6 +21,53 @@ namespace macro {
  * to support R7RS.
  */
 class SyntaxRulesMacro {
+
+public:
+  /**
+   * @brief The Macro class used to match and expand against a single
+   * SyntaxRules Macro.
+   * @param macro_keyword Identifier used to represent macro keyword
+   * @param syntax_cases Vector of SyntaxRule instances used for case
+   * matching and expanding.
+   */
+  SyntaxRulesMacro(
+      Symbol& macro_keyword,
+      std::vector<SyntaxRulePtr>& syntax_cases
+  );
+
+  /**
+   * @brief Match to and expand a macro use. Used in macro expander
+   * preprocessor to modify a the AST in-place.
+   * Can potentially throw a MacroExpansionException.
+   * @param macro The CDR of the macro expression.
+   * @return true if successfully matched and replaced, false otherwise.
+   */
+  bool transform(NodePtr macro);
+
+  /**
+   * @brief Prints out a short representation of the SyntaxRulesMacro.
+   * @param lhs The output stream.
+   * @param rhs  The SyntaxRulesMacro.
+   * @return the updated reference of the output stream.
+   */
+  friend std::ostream& operator<<(
+      std::ostream& lhs,
+      const SyntaxRulesMacro& rhs
+  );
+
+private:
+  /**
+   * @brief The identifier for this macro.
+   * TODO: Consider if this is necessary.
+   */
+  const Symbol macro_keyword;
+
+  /**
+   * @brief Pointers to instances of SyntaxCases used for matching and
+   * expansion of individual cases.
+   */
+  const std::vector<SyntaxRulePtr> syntax_rules;
+
 };
 
 /**
